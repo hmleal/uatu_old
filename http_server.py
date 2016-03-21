0bfa3c0af4eb94dbc30524651b0c7ed917834e34
Jorge Luiz <jorgeltpf@hotmail.com>
2016-03-21 17:14:51 -0300

    Implementação da função request_parse.

--- http_server.py
+++ http_server.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 import socket
 
-
 SOCK_ADDRESS = ('127.0.0.1', 9000)
 
+def request_parse(text):
+    # TODO implementar o parse do texto aqui
+    request_line = text.splitlines()[0]
+    request_line = request_line.rstrip('\r\n')
+    request_method = request_line.split()
+
+    server_line = text.splitlines()[1]
+    server_line = server_line.rstrip('\r\n')
+    server_name = server_line.split()
+
+    env = {}
+    env['REQUEST_METHOD'] = request_method[0]
+    env['PATH_INFO'] = request_method[1]
+    env['SERVER_NAME'] = server_name[1]
+    env['SERVER_PORT'] = 9000
+    return env
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,7 +35,7 @@ if __name__ == '__main__':
         connection, address = sock.accept()
         request = connection.recv(1024)
 
-        print(request)
+        print(request_parse(request))
 
         if not request:
             break
